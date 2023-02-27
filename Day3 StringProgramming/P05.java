1650c6e759ab75789dd23b010ce7352e883de40c
Abhijeet Dhumal <dhumalabhi60@gmail.com>
2023-02-28 02:39:17 +0530

    Day 3 que 5

--- Day3 StringProgramming/P05.java
+++ Day3 StringProgramming/P05.java
@@ -11,12 +11,18 @@ class P05{
 		for(int i=0;i<str.length();i++){
 			char ch=str.charAt(i);
 			switch(ch){
-				case 'a':
-				case 'i':
-				
-				
+				case 'A':	count++; break;
+				case 'a':	count++; break;
+				case 'I':	count++; break;
+				case 'i':	count++; break;
+				case 'E':	count++; break;
+				case 'e':	count++; break;
+				case 'O':	count++; break;
+				case 'o':	count++; break;
+				case 'U':	count++; break;
+				case 'u':	count++; break;
 			}
 		}
-		
+		System.out.print(count);
 	}
 }

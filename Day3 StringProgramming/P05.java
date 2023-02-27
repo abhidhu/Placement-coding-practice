60dc4b275326c96eaf3c2ed8ddc97dd4e37be254
Abhijeet Dhumal <dhumalabhi60@gmail.com>
2023-02-28 00:22:59 +0530

    Update P05.java

--- Day3 StringProgramming/P05.java
+++ Day3 StringProgramming/P05.java
@@ -5,6 +5,18 @@ import java.util.Scanner;
 
 class P05{
 	public static void main(String[] args){
-	
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		int count=0;
+		for(int i=0;i<str.length();i++){
+			char ch=str.charAt(i);
+			switch(ch){
+				case 'a':
+				case 'i':
+				
+				
+			}
+		}
+		
 	}
 }

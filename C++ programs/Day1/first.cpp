9d19d48cbd776bb23923594b8ef94fbbfd111a27
Abhijeet Dhumal <dhumalabhi60@gmail.com>
2023-03-07 22:23:32 +0530

    C++ coding

---
+++ C++ programs/Day1/first.cpp
@@ -0,0 +1,14 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+int main()
+{
+	
+	int x;
+	cout<<"Enter a number "<<endl;
+	cin>>x;
+	int s=x*x;
+	cout<<"Squere is "<<s<<endl;
+	getch();
+	return 0;
+}
---
+++ C++ programs/Day1/func.cpp
@@ -0,0 +1,19 @@
+#include<conio.h>
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    void fun(); // Declaration
+
+    cout<<"we are inside the main\n";
+
+    fun();
+
+}
+
+void fun()
+{
+    cout<<"inside the fun \n";
+}
